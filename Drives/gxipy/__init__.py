90043523781d91b4f13a961049189507e8edb78a
AHAO <108380260+AHaoI111@users.noreply.github.com>
2025-05-19 18:04:50 +0800

    Add files via upload

---
+++ Drives/gxipy/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+# -*-mode:python ; tab-width:4 -*- ex:set tabstop=4 shiftwidth=4 expandtab: -*-
+
+from Drives.gxipy.gxiapi import *
+from Drives.gxipy.gxidef import *
+
+
+__all__ = ["gxwrapper", "dxwrapper", "gxiapi", "gxidef"]
+
+__version__ = '2.0.2105.9261'
